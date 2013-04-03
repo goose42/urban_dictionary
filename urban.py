dc6bb61460bb5bfe8f42811c4673118e8db3cd89
Jayanth <vjayanth@indiana.edu>
2013-04-03 15:17:05 -0400

    milti word query

--- urban.py
+++ urban.py
@@ -3,8 +3,20 @@
 from dictionary import urban_diction
 import sys
 
-query = sys.argv[1]
- 
+if len(sys.argv) < 2:
+  print "Use the correct syntax. ./urban <query string>"
+  sys.exit(0)
+not_first = 1
+search_word = ""
+space = ""
+for word in sys.argv:
+  if not_first > 1:
+    search_word += space + word
+    space = " "
+  not_first +=1
+
+query = search_word
+
 urban = urban_diction()
 
 urban.search(query)
